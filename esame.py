42123ded64fdebd6ecf9fdb5dc516423fde1fbcf
Ludovico Gardenal <100026332+Ludovico02@users.noreply.github.com>
2022-02-20 17:15:14 +0000

    Aggiunte varie

--- esame.py
+++ esame.py
@@ -1,9 +1,7 @@
 '''
 - se il file è vuoto semplicemente non sono presenti gli anni in time_series quindi si alza un eccezzione
-- timestamp duplicato 
-- la data deve avere il - ?
+- la data deve avere il -
 '''
-# import re
 
 class ExamException(Exception):
     pass # raise ExamException('Errore')
@@ -25,7 +23,7 @@ class CSVTimeSeriesFile():
             elements = line.split(",")
             
             elements = [element.replace(" ", "").strip() for element in elements]
-
+            # cambia
             if len(elements) > 2:
                 aux, num = "", ""
                 data_found, num_found = False, False
@@ -37,27 +35,45 @@ class CSVTimeSeriesFile():
                         num_found = True
                         num = element
                 elements = [aux, num]
-
-            # rimuovo eventuali spazi o "a capo" dagli elementi
-            # elements[-1] = elements[-1].strip()
-            # elements[0] = elements[0].strip()
-
-            try:
-                elements[-1] = int(elements[-1])
-            except:
-                # dato che il numero non è convertibile in intero, il numero è una str
-                elements[-1] = None
-
-            if elements[-1] != None and elements[-1] <= 0: # se il valore è minore di 0 o nullo (= 0)
-                elements[-1] = None
-
-            if len(elements) < 2:
-                elements.insert(0, "")
         
             # if re.match("[0-9\-]*$", elements[0]): # aggiungere verifiche?
             #     data.append(elements)
-            if "-" in elements[0] and elements[0].replace("-", "").isnumeric():
-                data.append(elements)
+
+            if elements[0].count("-") == 1 and elements[0].replace("-", "").isnumeric() and len(elements) == 2:
+                
+                try:
+                    elements[-1] = int(elements[-1])
+                except:
+                    # dato che il numero non è convertibile in intero, il numero è una str
+                    elements[-1] = None
+
+                if elements[-1] != None and elements[-1] <= 0:
+                    elements[-1] = None
+                
+                verify_month, verify_year = True, True
+
+                # verifica del mese
+                try:
+                    month = int(elements[0][elements[0].find("-") + 1:])
+                except:
+                    verify_month = False
+                if  month > 12 or month < 1:
+                    verify_month = False
+
+                # verifica dell' anno
+                try:
+                    year = int(elements[0][:elements[0].find("-")])
+                except:
+                    verify_year = False
+                if year <= 0:
+                    verify_year = False
+                if verify_year and verify_month:
+                    dupl = False
+                    for el in data:
+                        if elements[0] in el:
+                            dupl = True
+                    if not dupl:
+                        data.append(elements)
             
         my_file.close()
         return data
@@ -92,30 +108,48 @@ def detect_similar_monthly_variations(time_series, years):
         raise ExamException("Uno dei due anni non è presente nella lista")
 
     first, second = [], []
+    first_months, second_months = [], []
     for list in time_series:
         if str(years[0]) in list[0]:
             first.append(list[1])
+            first_months.append(int(list[0][list[0].find("-") + 1:]))
         if str(years[1]) in list[0]:
+            second_months.append(int(list[0][list[0].find("-") + 1:]))
             second.append(list[1])
     print(first)
     print(second)
+    print(first_months)
+    print(second_months)
+
+    for i in range(len(first_months) - 1):
+        if i == 0 and first_months[i] != 1:
+            for j in range(first_months[i] - 1):
+                first.insert(j, None)
+        if first_months[i] != first_months[i + 1] - 1:
+            first.insert(first_months[i], None)
+    if first_months[-1] != 12:
+        for i in range(12 - first_months[-1]):
+            first.append(None)
+
+    for i in range(len(second_months) - 1):
+        if i == 0 and second_months[i] != 1:
+            for j in range(second_months[i] - 1):
+                second.insert(j, None)
+        if second_months[i] != second_months[i + 1] - 1:
+            second.insert(second_months[i], None)
+    if second_months[-1] != 12:
+        for i in range(12 - second_months[-1]):
+            second.append(None)
+
+    print("primo =", first, len(first))
+    print("secondo =", second, len(second))
 
     first = [None if first[i] == None or first[i + 1] == None else abs(first[i] - first[i + 1]) for i in range(len(first) - 1)]
-    # for i in range(len(first) - 1):
-    #     if first[i] == None or first[i + 1] == None:
-    #         first[i] = None
-    #     else:
-    #         first[i] = abs(first[i] - first[i + 1])
-    # first.pop()
+
     second = [None if second[i] == None or second[i + 1] == None else abs(second[i] - second[i + 1]) for i in range(len(second) - 1)]
-    # for i in range(len(second) - 1):
-    #     if second[i] == None or second[i + 1] == None:
-    #         second[i] = None
-    #     else:
-    #         second[i] = abs(second[i] - second[i + 1])
-    # second.pop()
-    print(first)
-    print(second)
+
+    print("p2 =", first, len(first))
+    print("s2 =", second, len(second))
 
     finale = []
     for x, y in zip(first, second):
@@ -125,7 +159,7 @@ def detect_similar_monthly_variations(time_series, years):
             finale.append(True)
         else:
             finale.append(False)
-    for i in range(12 - len(finale)):
+    for i in range(11 - len(finale)):
         finale.append(False)
     print(finale, len(finale)) # return
 
